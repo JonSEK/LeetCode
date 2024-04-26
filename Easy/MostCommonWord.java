eeb23436f81b1485954ab551af12607043d9b9ed
JonSEK <engkien@ymail.com>
2024-04-26 10:21:17 +0800

    Given a string paragraph and a string array of the banned words banned, return the most frequent word that is not banned.

---
+++ Easy/MostCommonWord.java
@@ -0,0 +1,42 @@
+/*
+Given a string paragraph and a string array of the banned words banned, return the most frequent word that is not banned.
+It is guaranteed there is at least one word that is not banned, and that the answer is unique.
+The words in paragraph are case-insensitive and the answer should be returned in lowercase.
+
+Example 1:
+Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
+Output: "ball"
+Explanation:
+    "hit" occurs 3 times, but it is a banned word.
+    "ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph.
+Note that words in the paragraph are not case sensitive,
+that punctuation is ignored (even if adjacent to words, such as "ball,"),
+and that "hit" isn't the answer even though it occurs more because it is banned.
+
+Example 2:
+Input: paragraph = "a.", banned = []
+Output: "a"
+*/
+
+import java.util.*;
+
+public class MostCommonWord {
+  public static void main(String[] args) {
+    String paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+    String[] banned = {"hit"};
+    System.out.println(mostCommonWord(paragraph, banned));
+  }
+
+  public static String mostCommonWord(String paragraph, String[] banned) {
+    Set<String> bannedWords = new HashSet<>();
+    Collections.addAll(bannedWords, banned);
+    Map<String, Integer> wordCount = new HashMap<>();
+    String[] words = paragraph.replaceAll("[^a-zA-Z ]", " ").toLowerCase().split("\\s+");
+    for (String word : words) {
+      if (!bannedWords.contains(word)) {
+        wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+      }
+    }
+    return Collections.max(wordCount.entrySet(), Map.Entry.comparingByValue()).getKey();
+  }
+}
